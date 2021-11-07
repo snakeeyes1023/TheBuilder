a0972de7987eae531e90cad92a34e9ff318cea00
TheArchightech <71300736+TheArchightech@users.noreply.github.com>
2021-11-06 21:36:54 -0400

    Creation market (#22)
    * Ajout des menu, l'option pause + début market pour tours
    
    * Création du market pour les tours + début de tour

---
+++ Scripts/Tours/market.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var Tour_de_Base = preload("res://Scenes/Tours/Tour de base.tscn")
+
+export var argent = 100
+
+func _on_TextureButton1_pressed():
+	if argent >= 100:
+		print("test")
+		var instance = Tour_de_Base.instance()
+		get_parent().add_child(instance)
+
+
+func _on_TextureButton2_pressed():
+	if argent >= 200:
+		print("tour 200$")
+
+
+func _on_TextureButton3_pressed():
+	if argent >= 300:
+		print("tour 300$")
+
+func changer_argent(argent_changement):
+	argent += argent_changement
+	$VBoxContainer/LabelArgents.text = "Argent: " + str(argent) + "$"
