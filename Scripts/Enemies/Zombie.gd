eeff8dc9151e3ef65061330480ec7590648f626a
Jonathan <cotejonathan638@gmail.com>
2021-11-08 22:06:16 -0500

    Ajout des enemies

---
+++ Scripts/Enemies/Zombie.gd
@@ -0,0 +1,9 @@
+extends "res://Scripts/Enemies/EnemieBase.gd"
+
+export (int) var point_de_vie = 3
+export (int) var vitesse = 70
+
+#Initialisation de la classe parent
+func _init().(vitesse, point_de_vie):
+	pass
+	
