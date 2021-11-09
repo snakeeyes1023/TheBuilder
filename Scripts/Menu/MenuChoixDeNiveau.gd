199dba438c715590a0220603967ccc70a94e4122
TheArchightech <2034909@carrefour.cegepvicto.ca>
2021-11-09 15:42:56 -0500

    Amilioration market + correction des noms de menu

---
+++ Scripts/Menu/MenuChoixDeNiveau.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_ButtonDidactitiel_pressed():
+	get_tree().change_scene("res://Scenes/jeu.tscn")
+
+
+func _on_ButtonLvl1_pressed():
+	pass # Replace with function body.
+
+
+func _on_ButtonLvl2_pressed():
+	pass # Replace with function body.
--- Scripts/Menu/PauseMenu.gd
+++ Scripts/Menu/PauseMenu.gd
@@ -16,22 +16,21 @@ func _process(_delta):
 			self.visible = false
 
 
-func _on_Button_pressed():
+func _on_ButtonReprendre_pressed():
 	menu_escape_visible = false
 	get_tree().paused = false
 	self.visible = false
 
 
-func _on_Button2_pressed():
+func _on_ButtonRecommencer_pressed():
+	get_parent().get_tree().reload_current_scene()
+	get_tree().paused = false
+	self.hide()
+
+func _on_ButtonMenu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene("res://Scenes/Menu/MenuPrincipale.tscn")
 
 
-func _on_Button3_pressed():
+func _on_ButtonQuitter_pressed():
 	get_tree().quit()
-
-
-func _on_Button4_pressed():
-	get_parent().get_tree().reload_current_scene()
-	get_tree().paused = false
-	self.hide()
