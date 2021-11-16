5336a0b39877b793b2a21950b435796f631c4510
TheArchightech <71300736+TheArchightech@users.noreply.github.com>
2021-11-16 15:53:39 -0500

    Ajout sons (#38)

--- Scripts/Menu/MenuChoixDeNiveau.gd
+++ Scripts/Menu/MenuChoixDeNiveau.gd
@@ -3,12 +3,13 @@ extends Control
 
 
 func _on_ButtonDidactitiel_pressed():
+	InformationJeu.reinitialiser_vie()
 	get_tree().change_scene("res://Scenes/jeu.tscn")
 
 
 func _on_ButtonLvl1_pressed():
-	pass # Replace with function body.
+	InformationJeu.reinitialiser_vie()
 
 
 func _on_ButtonLvl2_pressed():
-	pass # Replace with function body.
+	InformationJeu.reinitialiser_vie()
